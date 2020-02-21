36087311fb64cc4c0c1fd8d16ff3caf78ab89b9a
ArtfulDodge <jcoffey97@gmail.com>
2020-02-21 10:22:44 -0500

    trees now get cut down when something walks above or below them

---
+++ Assets/Scripts/MonoBehaviours/TreeTopBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTopBehavior : MonoBehaviour
+{
+    public BoxCollider2D hitbox;
+    public BoxCollider2D triggerbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
