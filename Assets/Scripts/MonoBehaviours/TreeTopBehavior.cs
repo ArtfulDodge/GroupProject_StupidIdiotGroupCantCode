4916092183065c88e8bc02a1b9e4df2d4bd96977
coffejo <coffejo@bc.edu>
2020-02-24 13:26:43 -0500

    Added remaining trees counter and game over overlay, touched up tilemap, added score ui element with no functionality

--- Assets/Scripts/MonoBehaviours/TreeTopBehavior.cs
+++ Assets/Scripts/MonoBehaviours/TreeTopBehavior.cs
@@ -1,26 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreeTopBehavior : MonoBehaviour
 {
     public BoxCollider2D hitbox;
     public BoxCollider2D triggerbox;
+    public Text gameOverText;
+    public Text txtTreesLeft;
+    public static int treesLeft = 6;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        txtTreesLeft.text = "Remaining Trees: " + treesLeft.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(gameObject);
+        if (other.gameObject.tag == "Enemy")
+        {
+            DecreaseTrees();
+            txtTreesLeft.text = "Remaining Trees: " + treesLeft.ToString();
+            if (treesLeft <= 0)
+            {
+                GameOver();
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    static void DecreaseTrees()
+    {
+        treesLeft--;
+    }
+
+    void GameOver()
+    {
+        gameOverText.text = "Game Over";
     }
 }
