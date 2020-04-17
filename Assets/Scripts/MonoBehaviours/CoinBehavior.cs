07ad42fd50785f8913be1936aab7aff0bf973e75
ArtfulDodge <jcoffey97@gmail.com>
2020-04-17 02:50:36 -0400

    you can now move the tower, adjusted enemy and wave spawn times, added tower levels

--- Assets/Scripts/MonoBehaviours/CoinBehavior.cs
+++ Assets/Scripts/MonoBehaviours/CoinBehavior.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class CoinBehavior : MonoBehaviour
 {
-    public BoxCollider2D triggerbox;
-    public BoxCollider2D hitbox;
     public Text txtCoinsCollected;
     public static int coinsGathered = 0;
 
     void Start()
+    {
+        txtCoinsCollected.text = "Coins: " + coinsGathered.ToString();
+    }
+
+    void Update()
     {
         txtCoinsCollected.text = "Coins: " + coinsGathered.ToString();
     }
